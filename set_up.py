5dab73fe56926c2aed5e0b5417c6182decde4d09
SysMednet <87461989+SysMednet@users.noreply.github.com>
2024-01-18 14:12:15 +0800

    Add files via upload

---
+++ set_up.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+requirements = [
+    'numpy',
+    'pandas',
+    'os',
+    'scipy',
+    'statsmodels',
+    'matplotlib',
+    'seaborn',
+    'shutil',
+]
+
+setup(
+    name='Gscore',
+    version='0.0.1',
+    description="GscorePy is a python implementation of Gscore.",
+    author="SysMednet",
+    author_email='medsb.net@gmail.com',
+    url='https://github.com/SysMednet/GscorePy',
+    install_requires=requirements,
+    packages=[
+        'Gscore',
+    ],
+    package_dir={'Gscore': 'Gscore'},
+    package_data={'Gscore':['Database/*/*/*','Database/*/*','Database/*']},
+    license="MIT license",
+    zip_safe=True,
+    keywords='Gscore',
+    classifiers=[
+        'Programming Language :: Python :: 3.9.13',
+    ],
+)
