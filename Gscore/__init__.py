f9e8b66a793b5428c8e33a604452b966d21b58ea
SysMednet <87461989+SysMednet@users.noreply.github.com>
2024-01-19 13:46:55 +0800

    Update __init__.py

--- Gscore/__init__.py
+++ Gscore/__init__.py
@@ -1,6 +1,4 @@
-'''from .Gscore import Gscore
-from . import coexpressed_detail
-from . import coexpression
-from . import dotplot
-from . import main_process
-from . import network_graph'''
+from .Gscore import Gscore
+
+__all__ = ['Gscore']
+
