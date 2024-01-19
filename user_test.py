922d8a18533f146b40fc1bd656a13df38c0b548b
SysMednet <87461989+SysMednet@users.noreply.github.com>
2024-01-19 14:10:01 +0800

    Update user_test.py

--- user_test.py
+++ user_test.py
@@ -1,13 +1,13 @@
-import Gscore as gs
+from gscorepy import gscore
 
-gs.Gscore(GEM='test_input/GSE157103.txt', 
-            DEG='test_input/allDEG.txt', 
-            pcc_cutoff=0.7, 
-            criterion='fdrq', 
-            gene_set='KEGG', 
-            species='hsa', 
-            ID_type='entrez',
-            output_dir = 'test_output/',
-            coexp_detail=True,
-            ratio_plot=True,
-            coexp_graph=True)
+gscore(GEM='test_input/GSE157103.txt', 
+        DEG='test_input/allDEG.txt', 
+        pcc_cutoff=0.7, 
+        criterion='pv', 
+        gene_set='KEGG', 
+        species='hsa', 
+        ID_type='entrez',
+        output_dir = 'test_output/',
+        coexp_detail=True,
+        ratio_plot=True,
+        coexp_graph=True)
